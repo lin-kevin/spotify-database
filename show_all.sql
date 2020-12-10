209fde116afd88de7857c70405b95a620180cca2
Kevin Lin <linkevin@cmu.edu>
2020-12-10 22:25:31 +0100

    Start writing show_all.sql

---
+++ show_all.sql
@@ -0,0 +1,17 @@
+\c spotify
+
+\echo Album_Upload: the username of the artist corresponding to each album id
+SELECT * FROM Album_Upload;
+
+SELECT * FROM Album;
+SELECT * FROM Artist;
+SELECT * FROM Episode;
+SELECT * FROM Listen_Episode;
+SELECT * FROM Listen_Song;
+SELECT * FROM Listener;
+SELECT * FROM Playlist_Contains;
+SELECT * FROM Playlist;
+SELECT * FROM Podcast;
+SELECT * FROM Podcaster;
+SELECT * FROM Song;
+SELECT * FROM "User";
